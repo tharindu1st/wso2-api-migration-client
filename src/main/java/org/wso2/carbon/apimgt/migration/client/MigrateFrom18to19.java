c1f30fdc630fbddd7a51db155ad8f96cd1f6e98b
Madusanka <rmmpremaratne@gmail.com>
2015-04-21 16:46:38 +0530

    first commit

---
+++ src/main/java/org/wso2/carbon/apimgt/migration/client/MigrateFrom18to19.java
@@ -0,0 +1,41 @@
+/*
+ *  Copyright WSO2 Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.wso2.carbon.apimgt.migration.client;
+
+import org.wso2.carbon.apimgt.migration.client.internal.MigrationClient;
+
+public class MigrateFrom18to19 implements MigrationClient {
+    @Override
+    public void databaseMigration() {
+
+    }
+
+    @Override
+    public void swaggerResourceMigration() {
+
+    }
+
+    @Override
+    public void registryMigration() {
+
+    }
+
+    @Override
+    public void rxtMigration() {
+
+    }
+}
